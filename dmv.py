8d2eac4b704080f9d3efc5476028aab2a4a47f47
Dan Yang <56417240+ckive@users.noreply.github.com>
2022-03-10 15:04:45 -0600

    complete for deployment

--- dmv.py
+++ dmv.py
@@ -1,39 +1,64 @@
 import yagmail
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+import json
+import logging
+
+logger = logging.getLogger('dmv_logs')
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('scrapes.log')
+formatter = logging.Formatter('%(asctime)s:%(levelname)s: %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+with open("config.json", "r") as jsonfile:
+    config = json.load(jsonfile)
 
 driver = webdriver.Chrome()
 driver.get(
     'https://ilsosappt.cxmflow.com/Appointment/Index/dbe30824-497b-4325-8c5f-68b146a5e898'
 )
 #click "In Car Driving Test"
-icdt = driver.find_element_by_class_name("DataControlBtn")
+# icdt = driver.find_element_by_class_name("DataControlBtn")
+icdt = driver.find_element(by=By.CLASS_NAME, value="DataControlBtn")
+
 icdt.click()
 #click "Advance In-Car Appointments"
 driver.get(
     'https://ilsosappt.cxmflow.com/Appointment/Index/dbe30824-497b-4325-8c5f-68b146a5e898'
 )
-aica = driver.find_element_by_class_name("DataControlBtn")
+aica = driver.find_element(by=By.CLASS_NAME, value="DataControlBtn")
 aica.click()
 
 #check all shown locations
 driver.get(
     'https://ilsosappt.cxmflow.com/Appointment/Index/dbe30824-497b-4325-8c5f-68b146a5e898'
 )
-locations = driver.find_elements_by_class_name("DataControlBtnUnit")
+locations = driver.find_elements(by=By.CLASS_NAME, value="center-textDiv")
+
+found = []
 
 for location in locations:
-    dmv = location.find_elements_by_class_name("center-textDiv")[0]
-    if dmv.text.split("\n")[0] == "Chicago West":
-        print("FOUND IT!")
+    loc = location.text.split('\n')[0]
+    if loc in config['dmv_sites']:
+        found.append(loc)
+
+logger.debug('Desired DMV locations not found')
 
 # Send mail!
-receiver = "danyang2023@u.northwestern.edu"
-body = "Chicago North opened up: 'https://ilsosappt.cxmflow.com/Appointment/Index/dbe30824-497b-4325-8c5f-68b146a5e898'"
+if found:
+    try:
+        logger.debug('Appointments found at: {}, sending email'.format(found))
+        receiver = config['receiver']
+        body = "DMV opened up at {}: 'https://ilsosappt.cxmflow.com/Appointment/Index/dbe30824-497b-4325-8c5f-68b146a5e898'".format(
+            found)
 
-yag = yagmail.SMTP('ddysideacc@gmail.com',
-                   oauth2_file="~/Downloads/oauth2_creds.json")
-yag.send(
-    to=receiver,
-    subject="DMV Appointment Found",
-    contents=body,
-)
+        yag = yagmail.SMTP(user=config['user'], password=config['pass'])
+        #oauth2_file="~/Desktop/projects/dmv/oauth2_creds.json")
+        yag.send(
+            to=receiver,
+            subject="DMV Appointment Found",
+            contents=body,
+        )
+    except Exception as e:
+        logger.error('Email sending failed')
